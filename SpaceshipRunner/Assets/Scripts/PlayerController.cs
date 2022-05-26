7bc1167057d658052fb45e951afceb838a0d23fa
Drabbity <drabbity@protonmail.com>
2022-05-26 19:14:43 +0400

    Flip added
    Now the character can flip upside down while being on a platform. The gravity changes when he is flipped.

--- SpaceshipRunner/Assets/Scripts/PlayerController.cs
+++ SpaceshipRunner/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed = 10f;
     [SerializeField] private float _acceleration = 0.005f;
     [SerializeField] private float _maximumSpeedMultiplier = 1.5f;
+    [SerializeField] private float _groundedDistance = 0.05f;
     [SerializeField] private float _jumpForce = 10f;
     [SerializeField] private float _allowedJumpTime = 10f;
     [SerializeField] private float _gravity = 20f;
@@ -17,13 +18,19 @@ public class PlayerController : MonoBehaviour
     private BoxCollider2D _collider;
     private Animator _animator;
 
-    private Vector2 _velocity = default;
     private bool _hasJumpInput = false;
+    private bool _hasFlipInput = false;
+
+    private Vector2 _velocity = default;
     private float _jumpTime = 0f;
     private float _multiplier = 1f;
     private bool _isJumping = false;
     private bool _isGrounded = true;
-
+    
+    private Vector3 _flipPosition = default;
+    private Quaternion _flipRotation = default;
+    private bool _isFlipped = false;
+    private int _flipMultiplier = 1;
 
     private void Start()
     {
@@ -39,6 +46,7 @@ public class PlayerController : MonoBehaviour
         CalculateRun();
         CalculateGravity();
         CalculateJump();
+        CalculateFlip();
 
         SetAnimations();
         ExecuteMovements();
@@ -49,6 +57,7 @@ public class PlayerController : MonoBehaviour
     {
         _isGrounded = IsGrounded();
         _hasJumpInput = InputManager.Instance.IsKeyHolded("Jump");
+        _hasFlipInput = InputManager.Instance.IsKeyPressed("Flip");
     }
     private void CalculateRun()
     {
@@ -58,13 +67,7 @@ public class PlayerController : MonoBehaviour
     {
         if(!_isGrounded)
         {
-            _velocity.y -= _gravity * Time.deltaTime * _multiplier;
-
-            RaycastHit2D raycastHit = Physics2D.BoxCast(_collider.bounds.center, _collider.bounds.size, 0, Vector2.down, _velocity.y, _groundMask);
-            if(raycastHit.collider != null)
-            {
-                _velocity.y = raycastHit.distance;
-            }
+            _velocity.y -= _gravity * Time.deltaTime * _multiplier * _flipMultiplier;
         }
         else
         {
@@ -78,37 +81,71 @@ public class PlayerController : MonoBehaviour
         {
             _isJumping = true;
             _jumpTime = 0f;
-            _velocity.y = _jumpForce * _multiplier;
+            _velocity.y = _jumpForce * _multiplier * _flipMultiplier;
         }
         else if(!_isGrounded && _hasJumpInput && _jumpTime < _allowedJumpTime && _isJumping)
         {
             _jumpTime += Time.deltaTime * _multiplier;
-            _velocity.y = _jumpForce * _multiplier;
+            _velocity.y = _jumpForce * _multiplier * _flipMultiplier;
         }
         else if(!_isGrounded && !_hasJumpInput)
         {
             _isJumping = false;
         }
     }
+    private void CalculateFlip()
+    {
+        if (_hasFlipInput && _isGrounded && !_isJumping) 
+        {
+            CalculateFlipPosition();
+            CalculateFlipRotation();
+            _isFlipped = !_isFlipped;
+            _flipMultiplier *= -1;
+        }
+    }
+    private void CalculateFlipPosition()
+    {
+        var groundRayCast = GetGround(_groundedDistance);
+        float newYPositionShift = (_collider.bounds.size.y + groundRayCast.collider.bounds.size.y) * -_flipMultiplier;
+        _flipPosition = transform.position;
+        _flipPosition.y += newYPositionShift;
+    }
+
+    private void CalculateFlipRotation()
+    {
+        _flipRotation = transform.rotation;
+        _flipRotation.x = 180f * (_isFlipped ? 0 : 1);
+    }
+
     private void SetAnimations()
     {
         _animator.SetBool("IsGrounded", _isGrounded);
     }
     private void ExecuteMovements()
     {
+        if (_hasFlipInput && _isGrounded && !_isJumping)
+        {
+            transform.position = _flipPosition;
+            transform.rotation = _flipRotation;
+        }
+
         _rigidbody.velocity = _velocity;
     }
-    private bool IsGrounded()
-    {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(_collider.bounds.center, _collider.bounds.size, 0, Vector2.down, 0.1f, _groundMask);
-        return raycastHit.collider;
-    }
-
     private void AcceleratePlayer()
     {
-        if(_multiplier < _maximumSpeedMultiplier)
+        if (_multiplier < _maximumSpeedMultiplier)
         {
             _multiplier += _acceleration * Time.deltaTime;
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return GetGround(_groundedDistance).collider;
+    }
+
+    private RaycastHit2D GetGround(float distance)
+    {
+        return Physics2D.BoxCast(_collider.bounds.center, _collider.bounds.size, 0, (_isFlipped ? Vector2.up : Vector2.down), distance, _groundMask);
     }
 }
